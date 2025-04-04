5e512d0ddefa17c17d4e62d8ebd2c61b95099833
daguilar2004 <118065951+daguilar2004@users.noreply.github.com>
2025-04-04 06:07:25 -0500

    added hero image to team page and added SHPE and UTD colors to tailwind(see tailwind confi)

--- src/components/common/OfficerCard.jsx
+++ src/components/common/OfficerCard.jsx
@@ -9,10 +9,10 @@ const OfficerCard = ({ image, name, title, description }) => {
       <img
         src={image}
         alt={`${name}'s headshot`} // Added alt text for better accessibility
-        className="w-32 h-32 object-cover rounded-full mb-4 border-4 border-indigo-500"
+        className="w-32 h-32 object-cover rounded-full mb-4 border-4 border-shpe-blue shadow-lg"
       />
       <h3 className="text-xl font-semibold">{name}</h3>
-      <p className="text-indigo-600 font-medium">{title}</p>
+      <p className="text-shpe-blue font-medium">{title}</p>
       <p className="text-gray-600 mt-2">{description}</p>
     </div>
   );
