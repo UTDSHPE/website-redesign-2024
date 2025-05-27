d651bbbe3eab49f82089e6a7dce48f4ba012832e
rudyg <rudynoot123@gmail.com>
2025-05-26 22:51:28 -0500

    netlify function not working see if this will fix

--- functions/gcal.js
+++ functions/gcal.js
@@ -1,8 +1,6 @@
-// use serverless function within netlify to pull google calendar events from API
-export default async (req, res) => {
+export default async (req, context) => {
     const apiKey = process.env.VITE_APP_GCAL_API_KEY;
     const calendarId = 'utdshpe@gmail.com';
-
     const url = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${apiKey}`;
 
     try {
@@ -11,9 +9,17 @@ export default async (req, res) => {
             throw new Error(`Google API error: ${response.statusText}`);
         }
         const data = await response.json();
-        return res.status(200).json(data.items); // Return just the events array
+
+        return {
+            statusCode: 200,
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data.items),
+        };
     } catch (error) {
-        return res.status(500).json({ error: error.message });
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message }),
+        };
     }
 };
   
